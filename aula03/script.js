67f4f0447c5b537a1cabaaee46d4e4d6b86e6dea
Leandro Sartor <leandrosartorp@gmail.com>
2025-03-10 19:37:32 -0300

    aula03

--- aula03/script.js
+++ aula03/script.js
@@ -2,22 +2,22 @@ function adicionarTarefa() {
     let ulListaTarefas = document.getElementById("ulListaTarefas")
     let inputTarefa = document.getElementById("inputTarefa")
 
-    let tarefa = inputTarefa.value
-    let mensagem = "Tarefa adicionada com sucesso!"
-
-
+    let tarefa = inputTarefa.value.trim()
+    let pmensagem = document.getElementById("mensagem")
 
     if (tarefa.length === 0){
-        mensagem = "Digite um tarefa válida "
+        pmensagem.textContent = "Digite um tarefa válida "
+        pmensagem.style.color = "red"
     }
     else {
+        pmensagem.textContent = "Tarefa adicionada com sucesso!"
+        pmensagem.style.color = "green"
 
         let liNovaTarefa = document.createElement("li")
         liNovaTarefa.textContent = tarefa
         ulListaTarefas.appendChild(liNovaTarefa)
     }
 
-    document.getElementById("mensagem").textContent = mensagem
     inputTarefa.value = ""
     inputTarefa.focus()
 }
